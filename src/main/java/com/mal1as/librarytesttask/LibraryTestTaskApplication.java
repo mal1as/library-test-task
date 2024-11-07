fe0d5d5b52b23c90986328f9de7e0b27f99c4358
mal1as <mishadyakonov14050@gmail.com>
2024-11-07 10:11:04 +0300

    Initial commit

---
+++ src/main/java/com/mal1as/librarytesttask/LibraryTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mal1as.librarytesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mal1as/librarytesttask/LibraryTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mal1as.librarytesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
